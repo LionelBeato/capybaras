7dccaff380780d38544cb4f7dff6d815b0d4b562
Lionel <lionelbeato@gmail.com>
2020-04-29 16:15:53 -0400

    adds more to coding examples

---
+++ week-2/day-8/Lesson/src/Lab/KeyValue.java
@@ -0,0 +1,21 @@
+package Lab;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class KeyValue {
+
+    public static void main(String[] args) {
+
+        Map<String, String> personInfo = new HashMap<String, String>();
+
+        personInfo.put("name", "Joan");
+        personInfo.put("age", "25");
+        personInfo.put("hometown", "Hartford");
+        personInfo.put("favorite food", "burgers");
+
+    }
+
+
+
+}
