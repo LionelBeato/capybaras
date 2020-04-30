509d37039b58f9e7d7e38b7cb5424fe815899861
Lionel <lionelbeato@gmail.com>
2020-04-30 14:03:08 -0400

    adds rough assignment

--- week-2/day-9/java/src/Assignment/Sorting.java
+++ week-2/day-9/java/src/Assignment/Sorting.java
@@ -1,4 +1,42 @@
 package Assignment;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+
 public class Sorting {
+
+    /*
+    *
+    *
+    *
+        -Create one array or ArrayList of 100 values, filled with math.random*100 in a for each loop.
+
+        -Sort the array/ArrayList (must be a harder sort option and must be able to explain what’s happening)
+    *
+    *
+    * */
+
+    public static void main(String[] args) {
+
+        LinkedList<Integer> myInts = new LinkedList<>();
+
+
+        ArrayList<Integer> myIntegers = new ArrayList<>(100);
+
+        for (int i = 0; i < 100; i++){
+            myIntegers.add(i, (int) (Math.random()*100));
+        }
+
+
+
+        System.out.println(myIntegers);
+
+        myIntegers.sort(null);
+
+        System.out.println(myIntegers);
+
+    }
+
+
+
 }
