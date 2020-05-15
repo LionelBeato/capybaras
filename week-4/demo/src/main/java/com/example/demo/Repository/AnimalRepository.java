03efbef525eb4bb0bc27b426347df206bd0d85c2
Lionel Beato <beato@lionel.works>
2020-05-15 12:41:03 -0400

    removes week-4 Project

--- week-4/demo/src/main/java/com/example/demo/Repository/AnimalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.Model.Animal;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnimalRepository extends JpaRepository<Animal, Long> {
-}
--- week-4/demo/src/main/java/com/example/demo/Repository/FruitRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repository;
-
-import com.example.demo.Model.Fruit;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FruitRepository extends JpaRepository<Fruit, Long> {
-
-
-}
